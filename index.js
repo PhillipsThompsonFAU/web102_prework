8edfb1613ce43b09013a4c92d74bf54c7f9ea0f7
Alice-watson <Alicewatson1998@gmail.com>
2026-01-21 12:06:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
-
+Test
 // create a new element to hold the name of the top pledge game, then append it to the correct element
 
 // do the same for the runner up item
